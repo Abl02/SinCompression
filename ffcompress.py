112b46dcebc4b0e26bd214deed12ca7d78bcbefc
Abel Ianovici <ianovici.abel04@gmail.com>
2024-11-04 21:27:52 +0100

    added some comments

--- ffcompress.py
+++ ffcompress.py
@@ -1,5 +1,13 @@
 #! /bin/python3
 
+"""
+Functions :
+  play()
+  showFFT()
+  showWave()
+  decompose()
+"""
+
 import os
 
 from src.sinSound import SIN
@@ -13,6 +21,9 @@ p = pyaudio.PyAudio()
 
 
 def play(i=""):
+    """
+    play a sound using PyAudio
+    """
     if i == "":
         print("Enter a built-in sound or a filepath to a .wav file")
     elif i == "sin":
@@ -20,6 +31,7 @@ def play(i=""):
         f = SIN()
         f.play(p)
     else:
+        # file
         if not os.path.isfile(i):
             print(f"The file '{i}' does not exist.")
         else:
@@ -27,7 +39,10 @@ def play(i=""):
             f.play(p)
 
 
-def showfft(i=""):
+def showFFT(i=""):
+    """
+    Plot the given sound Fast Fourier Transfrom function
+    """
     if i == "":
         print("Enter a built-in sound or a filepath to a .wav file")
     elif i == "sin":
@@ -44,6 +59,9 @@ def showfft(i=""):
 
 
 def showWav(i=""):
+    """
+    Plot the wave of the given sound
+    """
     if i == "":
         print("Enter a built-in sound or a filepath to a .wav file")
     elif i == "sin":
@@ -60,6 +78,10 @@ def showWav(i=""):
 
 
 def decompose(i=""):
+    """
+    Plot the given sound's elemental sinusoidal fucntions with the
+    help of the Fast Fourier Transform function
+    """
     # TO DO - in FILE class
     if i == "":
         print("Enter a built-in sound or a filepath to a .wav file")
@@ -84,5 +106,5 @@ def exit():
     p.terminate()
 
 
-DISPATCHER_P = {"showf": showfft, "show": showWav, "dec": decompose}
+DISPATCHER_P = {"showf": showFFT, "show": showWav, "dec": decompose}
 DISPATCHER_N = {"play": play}
