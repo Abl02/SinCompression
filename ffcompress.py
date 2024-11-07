3a6c74edb3ec0c6ba9218b13712f42d7ebbd1eb0
Abel Ianovici <ianovici.abel04@gmail.com>
2024-11-07 20:51:49 +0100

    Can store a file path now with setFile and added better error handling and combined common code into checkInput function

--- ffcompress.py
+++ ffcompress.py
@@ -2,10 +2,12 @@
 
 """
 Functions :
+  checkInput()
   play()
   showFFT()
   showWave()
   decompose()
+  setFile()
 """
 
 import os
@@ -19,62 +21,79 @@ import pyaudio
 
 p = pyaudio.PyAudio()
 
+# GLOBAL
+CURRENT_FILE = None
+FILE_NOT_EXIST = "The file '{}' does not exist."
+NO_FILE_ENTERED = "Enter a built-in sound or a filepath to a .wav file"
+ACTION_NOT_SUPPORTED = "'{}' is not a supported action"
+
+
+def checkInput(i, a):
+    if i == "":
+        if not CURRENT_FILE:
+            print(NO_FILE_ENTERED)
+            return 1
+        else:
+            checkInput(CURRENT_FILE, a)
+
+    elif i == "sin":
+        # for build-in sin function
+        f = SIN()
+        match a:
+            case "S":
+                f.play(p)
+            case "P":
+                f.plot(plt)
+            case "F":
+                f.plot(plt)
+            case "D":
+                print("Noting to do here")
+            case _:
+                print(ACTION_NOT_SUPPORTED.format(a))
+                return 1
+
+    else:
+        # file
+        if not CURRENT_FILE:
+            if not os.path.isfile(i):
+                print(FILE_NOT_EXIST.format(i))
+                return 1
+            else:
+                f = FILE(i)
+        else:
+            f = CURRENT_FILE
+        match a:
+            case "S":
+                f.play(p)
+            case "P":
+                f.plot(plt)
+            case "F":
+                f.plotFFT(plt)
+            case "D":
+                f.decompose(plt)
+            case _:
+                print(ACTION_NOT_SUPPORTED.format(a))
+
 
 def play(i=""):
     """
     play a sound using PyAudio
     """
-    if i == "":
-        print("Enter a built-in sound or a filepath to a .wav file")
-    elif i == "sin":
-        # for build-in sin function
-        f = SIN()
-        f.play(p)
-    else:
-        # file
-        if not os.path.isfile(i):
-            print(f"The file '{i}' does not exist.")
-        else:
-            f = FILE(i)
-            f.play(p)
+    checkInput(i, "S")
 
 
 def showFFT(i=""):
     """
     Plot the given sound Fast Fourier Transfrom function
     """
-    if i == "":
-        print("Enter a built-in sound or a filepath to a .wav file")
-    elif i == "sin":
-        # for build-in sin function
-        f = SIN()
-        f.plot(plt)
-    else:
-        # for file
-        if not os.path.isfile(i):
-            print(f"The file '{i}' does not exist.")
-        else:
-            f = FILE(i)
-            f.plotFFT(plt)
+    checkInput(i, "F")
 
 
 def showWav(i=""):
     """
     Plot the wave of the given sound
     """
-    if i == "":
-        print("Enter a built-in sound or a filepath to a .wav file")
-    elif i == "sin":
-        # for build-in sin function
-        f = SIN()
-        f.plot(plt)
-    else:
-        # for file
-        if not os.path.isfile(i):
-            print(f"The file '{i}' does not exist.")
-        else:
-            f = FILE(i)
-            f.plot(plt)
+    checkInput(i, "P")
 
 
 def decompose(i=""):
@@ -83,18 +102,20 @@ def decompose(i=""):
     help of the Fast Fourier Transform function
     """
     # TO DO - in FILE class
-    if i == "":
-        print("Enter a built-in sound or a filepath to a .wav file")
-    elif i == "sin":
-        # for build-in sin function
-        print("Noting to do here")
+    checkInput(i, "D")
+
+
+def setFile(i=""):
+    """
+    Set the current file
+    """
+    global CURRENT_FILE
+    if not os.path.isfile(i):
+        print(FILE_NOT_EXIST.format(i))
+        CURRENT_FILE = None
+        return 1
     else:
-        # for file
-        if not os.path.isfile(i):
-            print(f"The file '{i}' does not exist.")
-        else:
-            f = FILE(i)
-            f.decompose(plt)
+        CURRENT_FILE = FILE(i)
 
 
 """
@@ -107,4 +128,4 @@ def exit():
 
 
 DISPATCHER_P = {"showf": showFFT, "show": showWav, "dec": decompose}
-DISPATCHER_N = {"play": play}
+DISPATCHER_N = {"play": play, "set": setFile}
